cc1a2084af02a279d192ac07b9b0ca473b777634
DJWL <1602951127@qq.com>
2025-05-15 19:44:16 +0800

    feat: 去掉url

--- frontend/src/apps/home/index.tsx
+++ frontend/src/apps/home/index.tsx
@@ -4,7 +4,7 @@ import AppItem from './components/AppItem';
 function Home() {
   return (
     <FlexContainer>
-      <AppItem path="/115" iconUrl="https://img.djwl.top/icon/5.png" title="我的115" description="115应用设置" />
+      <AppItem path="/115" title="我的115" description="115应用设置" />
       <AppItem path="/job" title="自动任务" description="自动任务" />
     </FlexContainer>
   );
