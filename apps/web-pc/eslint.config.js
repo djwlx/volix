a5a78dfe20091a37ce31b1fe41a0485e69d42cfb
DJWL <58622651+djwlx@users.noreply.github.com>
2025-12-31 09:30:59 +0000

    test

--- apps/web-pc/eslint.config.js
+++ apps/web-pc/eslint.config.js
@@ -1,5 +1,4 @@
 import tseslint from 'typescript-eslint';
 import rootConfig from '../../packages/config/eslint/base'; // 根目录配置
-import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config({ ignores: ['dist'] }, rootConfig);
--- apps/web-pc/src/hooks/useAppConfig.ts
+++ apps/web-pc/src/hooks/useAppConfig.ts
@@ -3,7 +3,6 @@ import { useContext } from 'react';
 
 export function useAppConfig() {
   const config = useContext(AppConfigContext);
-
   return {
     ...config,
   };
