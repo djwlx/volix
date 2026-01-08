20e06f88fdf7fe5327413e595d433a2cd9616b2b
DJWL <58622651+djwlx@users.noreply.github.com>
2026-01-08 09:45:04 +0000

    feat: rule

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -17,6 +17,7 @@ const commitLintConfig: UserConfig = {
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
     'header-max-length': [2, 'always', 100],
+    'subject-english': [2, 'always'],
   },
   plugins: [
     {
