0b5a74b4257684f8ffdd4c39a52287f254349609
DJWL <58622651+djwlx@users.noreply.github.com>
2026-01-08 07:39:36 +0000

    feat: homepage

---
+++ apps/pc-web/src/apps/home/index.tsx
@@ -0,0 +1,11 @@
+import { Nav } from '@douyinfe/semi-ui';
+
+function HomeApp() {
+  return (
+    <div>
+      <Nav mode="horizontal" />
+    </div>
+  );
+}
+
+export default HomeApp;
--- apps/pc-web/src/layouts/router.tsx
+++ apps/pc-web/src/layouts/router.tsx
@@ -1,8 +1,9 @@
+import HomeApp from '@/apps/home';
 import { createBrowserRouter } from 'react-router';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello World</div>,
+    element: <HomeApp />,
   },
 ]);
