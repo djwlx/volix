da3556fa25b93338bd8432387c427c635fc47eb3
DJWL <58622651+djwlx@users.noreply.github.com>
2026-01-17 03:37:40 +0000

    feat: pic style

--- apps/web-pc/src/apps/pic/index.tsx
+++ apps/web-pc/src/apps/pic/index.tsx
@@ -2,6 +2,10 @@ import { Image } from '@douyinfe/semi-ui';
 import styles from './index.module.scss';
 
 function PicApp() {
-  return <Image className={styles.full} src={'/api/115/pic?mode=direct'} />;
+  return (
+    <div className={styles.full}>
+      <Image src={'/api/115/pic?mode=direct'} />
+    </div>
+  );
 }
 export default PicApp;
