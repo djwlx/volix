f3ee38a3443a62a54fa4e6ebe46ae8d230fa8ed1
DJWL <1602951127@qq.com>
2026-01-28 23:46:07 +0800

    fix: pic download

--- apps/web-pc/src/apps/pic/index.tsx
+++ apps/web-pc/src/apps/pic/index.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
 import { Image } from '@douyinfe/semi-ui';
 import styles from './index.module.scss';
 
 function PicApp() {
-  return <Image className={styles.full} src={'/api/115/pic?mode=direct'} />;
+  const [src, setSrc] = useState<string>('');
+
+  const fetchImg = async () => {
+    const res = await fetch('/api/115/pic?mode=direct');
+    const blob = await res.blob();
+    const objectUrl = URL.createObjectURL(blob);
+    setSrc(objectUrl);
+  };
+
+  useEffect(() => {
+    fetchImg();
+    return () => {
+      if (src) {
+        URL.revokeObjectURL(src);
+      }
+    };
+  }, []);
+
+  return src ? (
+    <Image
+      setDownloadName={() => {
+        return Date.now().toString();
+      }}
+      className={styles.full}
+      src={src}
+    />
+  ) : null;
 }
+
 export default PicApp;
